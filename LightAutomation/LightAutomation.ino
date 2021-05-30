be4f9fe75c57b5ca06cfbf804a3da06ac87f1125
Emmanuel-codes <jonathanemma121@gmail.com>
2021-05-30 01:48:35 +0000

    I decided to use two PIR sensors to cover the length of my room. Both of them woulr work in an OR gate configuration for trigger. I coded the test code

--- LightAutomation/LightAutomation.ino
+++ LightAutomation/LightAutomation.ino
@@ -1,16 +1,21 @@
-#define pir 8
-int pirState;
+#define pir1 8
+#define pir2 9
+int pirState1, pirState2;
+
+
 
 void setup(){
-  pinMode(pir, INPUT);
+  pinMode(pir1, INPUT);
+  pinMode(pir2, INPUT);
   Serial.begin(9600);
 }
 void loop(){
-  pirState = digitalRead(pir);
-  if (pirState == HIGH){
+  pirState1 = digitalRead(pir1);
+  pirState2 = digitalRead(pir2);
+  if (pirState1 == HIGH || pirState2 == HIGH){
     Serial.println("Motion detected");
   }
-  else if (pirState == LOW){
+  else if (pirState1 == LOW && pirState2 == LOW){
     Serial.println("None detected");
   }
   delay(10);
